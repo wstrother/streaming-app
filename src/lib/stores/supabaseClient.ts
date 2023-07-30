863beb2c707b46faddac39d5ab56a2b14f5fbcfe
Wyatt Strother <wstrother@users.noreply.github.com>
2023-07-30 00:50:34 -0400

    move types to lib

--- src/lib/stores/supabaseClient.ts
+++ src/lib/stores/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
-import type { Database } from '../../../types/supabase';
+import type { Database } from '$lib/types/supabase';
 
 export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
 
