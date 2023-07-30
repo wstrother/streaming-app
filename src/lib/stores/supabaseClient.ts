8b37d8aadae60037da6055827a39f934039aeedb
Wyatt Strother <wstrother@users.noreply.github.com>
2023-07-30 00:46:28 -0400

    testing type stuff

--- src/lib/stores/supabaseClient.ts
+++ src/lib/stores/supabaseClient.ts
@@ -1,4 +1,13 @@
 import { createClient } from "@supabase/supabase-js";
 import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
+import type { Database } from '../../../types/supabase';
 
-export const supabase = createClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
+export const supabase = createClient<Database>(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY)
+
+export const get_vars = async () => {
+    let query  = await supabase
+        .from('state_variables')
+        .select('*')
+    
+    console.log(query)
+}
