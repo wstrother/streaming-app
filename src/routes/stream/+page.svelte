d8a4b38c9494abc72e401b256974e1af079b380c
Wyatt Strother <wstrother@users.noreply.github.com>
2023-08-04 05:01:04 -0400

    fix url

--- src/routes/stream/+page.svelte
+++ src/routes/stream/+page.svelte
@@ -1,14 +1,14 @@
 <script lang='ts'>
     import { layoutTree } from "$lib/stores/layoutStore"
     import LayoutNode from "$lib/components/layoutNode.svelte"
-    import streamBG from "$lib/images/stream-bgg.png"
+    import streamBG from "$lib/images/stream-bg.png"
 
 </script>
 <div id="stream-layout-container">
     {#if streamBG }
         <img src={streamBG} alt="stream bg" />
     {/if}
-    
+
     {#each $layoutTree.nodes as node}
         <LayoutNode {node} />
     {/each}
