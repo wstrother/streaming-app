9a8678f54649117f20751ee9735ef120df4f76a7
Wyatt Strother <wstrother@users.noreply.github.com>
2023-08-05 06:13:22 -0400

    faux-bg for edit

---
+++ src/routes/stream/[[edit]]/+layout.svelte
@@ -0,0 +1,22 @@
+<script>
+	// import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
+    // import '@skeletonlabs/skeleton/styles/skeleton.css';
+	// import '../app.postcss';
+    export let data;
+</script>
+
+{#if data.edit}
+    <div id='faux-bg' />
+{/if}
+
+<slot />
+
+<style>
+    #faux-bg {
+        background-color: #333;
+        position: absolute;
+        width: 100vw;
+        height: 100vh;
+        z-index: -100;
+    }
+</style>
