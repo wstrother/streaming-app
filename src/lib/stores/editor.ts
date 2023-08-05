30edddee1043944d92a65881231fb3fc0248686e
Wyatt Strother <wstrother@users.noreply.github.com>
2023-08-05 06:24:08 -0400

    scale persists with local storage

--- src/lib/stores/editor.ts
+++ src/lib/stores/editor.ts
@@ -1,6 +1,8 @@
-import { writable } from "svelte/store"
+import { writable, type Writable } from "svelte/store"
 import type { LayoutNodeCls } from "$lib/classes/layoutTree"
+import { localStorageStore } from '@skeletonlabs/skeleton'
+
 
 export const activeNode = writable<LayoutNodeCls | null>()
 
-export const scalePercent = writable<number>(100)
+export const scalePercent: Writable<number> = localStorageStore('scalePercent', 100)
