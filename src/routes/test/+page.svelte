9d135593875d03f150d6c4160a1602208002cca5
Wyatt Strother <wstrother@users.noreply.github.com>
2023-08-22 18:59:33 -0400

    better support for node tree selection

---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Inner from "./inner.svelte";
+</script>
+
+
+<div class="flex">
+    <div id="outer-div" class="bg-slate-600 m-2 rounded">
+        <Inner />
+    </div>
+</div>
+
+<style>
+    #outer-div {
+        position: relative;
+        top: 20px; left: 30px;
+        width: 200px;
+        height: 200px;
+    }
+
+    #outer-div:hover {
+        outline: 5px solid gold
+    }
+</style>
