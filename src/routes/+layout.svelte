c78f7e357ed1f6afc35ea1aa9a91128a8dfb46b7
Wyatt Strother <wstrother@users.noreply.github.com>
2023-08-29 11:01:53 -0400

    update skeleton

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
-	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
-    import '@skeletonlabs/skeleton/styles/skeleton.css';
 	import '../app.postcss';
 	import { Modal } from '@skeletonlabs/skeleton';
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	initializeStores()
 
 </script>
 
