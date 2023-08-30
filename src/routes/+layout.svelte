eb50faf784edeea6bb93dfaffd9267b13c28891b
Wyatt Strother <wstrother@users.noreply.github.com>
2023-08-30 06:23:11 -0400

    nevermind the modal

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,13 @@
 <script lang='ts'>
-	import '../app.postcss';
-	import { Modal } from '@skeletonlabs/skeleton';
-	import type { ModalComponent } from '@skeletonlabs/skeleton';
-	import { initializeStores } from '@skeletonlabs/skeleton';
-	import NewNodePanel from '$lib/components/newNodePanel.svelte';
+	import '../app.postcss'
+	import { Modal } from '@skeletonlabs/skeleton'
+	// import type { ModalComponent } from '@skeletonlabs/skeleton'
+	import { initializeStores } from '@skeletonlabs/skeleton'
 	initializeStores()
 
-	const modalComponentRegistry: Record<string, ModalComponent> = {
-	NewNodePanel: {ref: NewNodePanel}
-	};
 
 </script>
 
-<Modal components={modalComponentRegistry}/>
+<Modal />
 
 <slot />
