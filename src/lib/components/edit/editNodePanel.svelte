2234320429312d40ecb553c3f1a756e875546fd8
Wyatt Strother <wstrother@users.noreply.github.com>
2023-08-31 13:00:13 -0400

    set parent id button

--- src/lib/components/edit/editNodePanel.svelte
+++ src/lib/components/edit/editNodePanel.svelte
@@ -1,15 +1,47 @@
 <script lang="ts">
+	import type { ProxyAttrs } from "$lib/classes/dbProxy";
 	import type { LayoutNodeProxy } from "$lib/classes/layoutNodes"
 	import EditProxyPanel from "./editProxyPanel.svelte"
+    import { getModalStore } from '@skeletonlabs/skeleton'
+    const modalStore = getModalStore()
 
     export let node: LayoutNodeProxy
+
+    let attrs: ProxyAttrs = [
+        'content',
+        ['top', 'number'],
+        ['left', 'number'],
+        'classes',
+        ['width', 'number'],
+        ['height', 'number'],
+    ]
+
+    const setParent = () => {
+        modalStore.trigger({
+            type: 'component',
+            component: 'fullNodeList',
+            meta: {
+                onClick: (e: CustomEvent) => {
+                    node.setColumn("parent_node_id", e.detail.id)
+                    modalStore.close()
+                },
+                exclude: [node]
+            }
+        })
+    }
 </script>
 
-<EditProxyPanel proxy={node} attrs={[
-    ['top', 'number'],
-    ['left', 'number'],
-    'content',
-    ['width', 'number'],
-    ['height', 'number']
-]}/>
+<EditProxyPanel proxy={node} {attrs}/>
 
+<div class="variant-glass-primary m-4 rounded p-3 text-white flex flex-col">
+    <button on:click={setParent}>Set Parent ID</button>
+    <button>Set Variable ID</button>
+    <button>Set Conditional Boolean ID</button>
+</div>
+
+<!-- svelte-ignore css-unused-selector -->
+<style lang='postcss'>
+    button {
+        @apply mb-1 btn variant-filled-primary
+    }
+</style>
--- src/lib/components/fullNodeList.svelte
+++ src/lib/components/fullNodeList.svelte
@@ -1,10 +1,33 @@
 <script lang="ts">
-    import { layoutNodes } from "$lib/classes/layoutNodes"
+    import { LayoutNodeProxy, layoutNodes } from "$lib/classes/layoutNodes"
 	import SelectProxyButton from "./selectProxyButton.svelte"
 
     import { activeProxyID } from "$lib/stores/editor"
     import { getModalStore } from '@skeletonlabs/skeleton'
     const modalStore = getModalStore()
+
+    let onClick: Function|null, onDblClick: Function|null
+    $: onClick = $modalStore[0]?.meta?.onClick ?? null
+    $: onDblClick = $modalStore[0]?.meta?.onDblClick ?? null
+
+    const _onClick = (e: CustomEvent) => {
+        if (onClick) onClick(e)
+        else activeProxyID.set(e.detail.id)
+    }
+
+    const _onDblClick = (e: CustomEvent) => {
+        if (onDblClick) onDblClick(e)
+        else modalStore.close()
+    }
+
+    let exclude: LayoutNodeProxy[] = []
+    $: exclude = $modalStore[0]?.meta?.exclude ?? []
+
+    let nodeList: LayoutNodeProxy[]
+    $: {
+        nodeList = $layoutNodes.filter(n => !n.parent_node_id)
+        if (exclude.length) nodeList = nodeList.filter(n => !exclude.includes(n))
+    }
 </script>
 
 <div id="full-nodes-panel" 
@@ -12,10 +35,10 @@
 
     <h1 class="h3 mb-2">Layout Nodes</h1>
 
-    {#each $layoutNodes.filter(n => !n.parent_node_id) as node}
+    {#each nodeList as node}
         <SelectProxyButton proxy={node} tree={true}
-            on:clickProxy={(e) => activeProxyID.set(e.detail.id)}
-            on:dblclickProxy={() => modalStore.close()}
+            on:clickProxy={_onClick}
+            on:dblclickProxy={_onDblClick}
         />
     {/each}
 </div>
