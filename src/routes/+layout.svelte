0e2da4f7cb927ed4fa7214f1e6d81406137a463e
Wyatt Strother <wstrother@users.noreply.github.com>
2023-08-31 12:20:13 -0400

    trying to make the transitions better

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,7 @@
 	import { Modal } from '@skeletonlabs/skeleton'
 	import type { ModalComponent } from '@skeletonlabs/skeleton'
 	import { initializeStores } from '@skeletonlabs/skeleton'
+	import { fade } from 'svelte/transition'
 	initializeStores()
 
 	import FullNodeList from '$lib/components/fullNodeList.svelte'
@@ -11,10 +12,10 @@
 		fullNodeList: {ref: FullNodeList},
 	}
 
-
 </script>
 
-<Modal {components} background="bg-primary-600" 
+<Modal {components} transitionIn={fade} transitionOut={fade}
+	background="bg-primary-600" 
 	regionBackdrop="variant-soft-secondary"
 	regionBody="text-white" 
 	regionHeader="text-white text-2xl font-bold"/>
