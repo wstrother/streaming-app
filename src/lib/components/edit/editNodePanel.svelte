53537d1d5e12af826ce02d0e24ca2903b0d24808
Wyatt Strother <wstrother@users.noreply.github.com>
2023-09-02 12:08:15 -0400

    css in actions panel

--- src/lib/components/edit/editNodePanel.svelte
+++ src/lib/components/edit/editNodePanel.svelte
@@ -20,20 +20,40 @@
 
 <EditProxyPanel proxy={node} {attrs}/>
 
-<div class="variant-glass-primary m-4 rounded p-3 text-white flex flex-col">
+<div class="variant-glass-primary m-4 rounded p-3 text-white flex flex-col actions-panel">
     <div>
+        <button on:click={() => setParentID(node, modalStore)}>Set Parent ID</button>
         {#if node.parent_node_id}
             <button on:click={() => unsetParentID(node)}>Unset Parent ID</button>
         {/if}
-        <button on:click={() => setParentID(node, modalStore)}>Set Parent ID</button>
     </div>
-    <button>Set Variable ID</button>
-    <button>Set Conditional Boolean ID</button>
+    <div>
+        <button>Set Variable ID</button>
+        {#if node.variable_id}
+            <button >Unset Variable ID</button>
+        {/if}
+    </div>
+    <div>
+        <button>Set Boolean ID</button>
+        {#if node.boolean_id}
+            <button >Unset Boolean ID</button>
+        {/if}
+    </div>
+    <div>
+        <button>Set Image</button>
+        {#if node.image}
+            <button >Unset Image</button>
+        {/if}
+    </div>
 </div>
 
 <!-- svelte-ignore css-unused-selector -->
 <style lang='postcss'>
     button {
-        @apply mb-1 btn variant-filled-primary
+        @apply mb-1 btn variant-filled-primary w-[100%]
+    }
+
+    .actions-panel div {
+        @apply flex justify-between
     }
 </style>
