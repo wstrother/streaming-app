8f85ab4dcf6e2cc7e55bca37aa939c1a1b0a75fd
Wyatt Strother <wstrother@users.noreply.github.com>
2023-09-11 16:13:10 -0400

    minimal reproduction of bug

--- src/routes/+layout.server.ts
+++
@@ -1,7 +0,0 @@
-import type { ServerLoadEvent } from "@sveltejs/kit"
-
-export const load = async ({ locals: { getSession } }:ServerLoadEvent) => {
-  return {
-    session: await getSession(),
-  }
-}
